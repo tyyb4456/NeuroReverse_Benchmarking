870accb8231bdef4f5f25b4eb91f55593e0f46a5
Tayyab <adanityb@gmail.com>
2025-02-23 10:07:33 +0500

    time

--- main.py
+++ main.py
@@ -182,4 +182,4 @@ def init_session():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port, timeout_keep_alive=420)
